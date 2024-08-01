ff8724017c1d88ddc5c97a638317eb4b7886921f
CHIGODI <chiegody254@gmail.com>
2024-08-01 08:10:30 +0300

    Filter datum

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Regex-ing """
+
+from typing import List
+import re
+
+
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Redacts the specified fields in a given message.
+    """
+    for field in fields:
+        pattern = rf'{field}=[^{separator}]+'
+        message = re.sub(pattern, f'{field}={redaction}', message)
+    return message
