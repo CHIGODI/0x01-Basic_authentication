021038ed9723285181df386a0e220cb01bb48f2a
CHIGODI <chiegody254@gmail.com>
2024-08-01 18:25:22 +0300

    Fix pycodestyle

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,9 +3,11 @@
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """Hash password"""
     return bcrypt.hashpw(password.encode('utf-8'),  bcrypt.gensalt())
+
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check if password macthes hashed password"""
