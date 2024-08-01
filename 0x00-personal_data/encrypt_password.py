619cdf76441fce4f59dbe113047e01020d9af8c0
CHIGODI <chiegody254@gmail.com>
2024-08-01 18:24:05 +0300

    Add task 5 and 6

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Encryption"""
+
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """Hash password"""
+    return bcrypt.hashpw(password.encode('utf-8'),  bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if password macthes hashed password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
