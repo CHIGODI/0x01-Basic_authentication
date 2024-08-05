cee03774f1f38eebad416254d609c743629619e4
CHIGODI <chiegody254@gmail.com>
2024-08-05 12:30:28 +0300

    Add task 3

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Auth """
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
+        """current_user"""
+        return None
