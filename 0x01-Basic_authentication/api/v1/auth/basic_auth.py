d1a35fd6e6a9fbf2e9e7a8936d9fd1a77d0c75e0
CHIGODI <chiegody254@gmail.com>
2024-08-06 16:09:20 +0300

    Add task 9: exctract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,3 +30,17 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str)\
+            -> (str, str):  # type: ignore
+        """exctracts user credentials"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        values = decoded_base64_authorization_header.split(':')
+        return values[0], values[1]
