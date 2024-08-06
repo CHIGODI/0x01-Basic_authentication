2a1bdf32ec3bf532cb77b360302e78f392aa3b00
CHIGODI <chiegody254@gmail.com>
2024-08-06 17:48:16 +0300

    Add return None

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -63,4 +63,5 @@ class BasicAuth(Auth):
             for user in users:
                 if user.is_valid_password(user_pwd):
                     return user
+                return None
         return None
