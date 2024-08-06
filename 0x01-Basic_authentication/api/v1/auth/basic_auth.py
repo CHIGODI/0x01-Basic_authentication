a8bc2cd90b5f840f97a0b3bba53e324f6fc9c6d5
CHIGODI <chiegody254@gmail.com>
2024-08-06 15:37:28 +0300

    Return correct slicing

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,12 +7,11 @@ class BasicAuth(Auth):
     """Basic Auth"""
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """extract base64 from header"""
+        """extract authorization header"""
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
             return None
         if not authorization_header.startswith('Basic '):
             return None
-
-        return authorization_header[len('Basic '):-1]
+        return authorization_header[len('Basic ')]
