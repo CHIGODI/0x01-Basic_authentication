c72bee72f3c6d46fe223e6a96f2a231c0d3e9b6d
CHIGODI <chiegody254@gmail.com>
2024-08-06 15:19:58 +0300

    Add task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,27 +7,25 @@ from typing import List, TypeVar
 
 class Auth:
     """Auth"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determines if the path requires authentication."""
-        # if path is None or excluded_paths is None or
-        # len(excluded_paths) == 0:
-        #     return True
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
 
-        # if path[-1] != '/':
-        #     path += '/'
+        if path[-1] != '/':
+            path += '/'
 
-        # if path in excluded_paths:
-        #     return False
-        # return True
-        return False
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header"""
-        # authorization = request.headers.get('Authorization')
-        # if request is None or authorization is None:
-        #     return None
-        # return authorization
-        return None
+        authorization = request.headers.get('Authorization')
+        if request is None or authorization is None:
+            return None
+        return authorization
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """current_user"""
