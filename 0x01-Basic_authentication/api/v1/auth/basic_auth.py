4b7b47122c2f6ff2914ea0e4c9bb4dc609cd7083
CHIGODI <chiegody254@gmail.com>
2024-08-06 17:31:14 +0300

    Add task 10: exctract user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Basic Auth"""
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -44,3 +46,21 @@ class BasicAuth(Auth):
 
         values = decoded_base64_authorization_header.split(':')
         return values[0], values[1]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str)\
+            -> TypeVar('User'):  # type: ignore
+        """return user instance based on email and passsword"""
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+
+        # fetch possible user object with email = email
+        users = User.search({'email': user_email})
+
+        if users and len(users) > 0:
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        return None
