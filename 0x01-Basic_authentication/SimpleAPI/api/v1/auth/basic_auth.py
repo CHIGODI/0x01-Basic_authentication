716f52007343a778f1a693bfec3cfdcbc23140f7
CHIGODI <chiegody254@gmail.com>
2024-08-06 07:54:27 +0300

    Remove cache files

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""""""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Auth"""
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """"""
+        if authorization_header is None:
+            return None
+        elif type(authorization_header) is not str:
+            return None
+        elif authorization_header[0:4] + '' != 'Basic ':
+            return None
+
+        return authorization_header[4:-1]
