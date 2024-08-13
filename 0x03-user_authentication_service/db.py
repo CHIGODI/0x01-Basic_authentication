236df771e5aae2223b31f7fa3197558673cd3be8
CHIGODI <chiegody254@gmail.com>
2024-08-13 13:59:47 +0300

    Add if statements to check inputs to add

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,8 @@ class DB:
 
     def add_user(self, email, hashed_password):
         """Add user to db"""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        if email and hashed_password:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+            return user
