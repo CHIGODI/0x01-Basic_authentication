ef4c5d7862e6c52ff7d2a29c4f9706e4e18e8ee6
CHIGODI <chiegody254@gmail.com>
2024-08-13 15:35:30 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,8 +2,26 @@
 """Auth module"""
 
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """Hash a password for storing."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def auth_register(self, email: str, password: str) -> User:
+        """"""
+        user = DB.find_user_by(email=email)
+        if user:
+            raise ValueError(f"User {email} already exists")
+        new_user = DB.add_user(email=email,
+                               password=_hash_password(password))
+        return new_user
