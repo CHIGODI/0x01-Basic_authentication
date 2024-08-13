d4626c9078fcba9973244cf2a9b2d3c50c91fb95
CHIGODI <chiegody254@gmail.com>
2024-08-13 08:59:04 +0300

    Add documentation to user class Fix pycodestyle

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,7 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
+    """User class"""
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
