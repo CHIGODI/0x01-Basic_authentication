8535c378cd8a449cab8e23ce00e025e9d9c57253
CHIGODI <chiegody254@gmail.com>
2024-08-14 08:45:13 +0300

    Add app with register user edpoint

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Flask App"""
+
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """return simple json"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """register a user."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if password and email:
+        try:
+            AUTH.auth_register(email,
+                               password)
+        except Exception:
+            return jsonify({"message": "email already registered"}), 400
+
+        return jsonify({"email": email,
+                        "message": "user created"})
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
