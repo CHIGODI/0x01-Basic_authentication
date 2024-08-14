1ef6baeb88c2cd6fa6d469dffedec141ead785a7
CHIGODI <chiegody254@gmail.com>
2024-08-14 10:55:10 +0300

    Credential validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -20,12 +20,24 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """Register a new user with an email and password."""
+        if isinstance(email, str) and isinstance(password, str):
+            try:
+                self._db.find_user_by(email=email)
+            except NoResultFound:
+                hashed_password = _hash_password(password)
+                new_user = self._db.add_user(email=email,
+                                             hashed_password=hashed_password)
+                return new_user
+            else:
+                raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if user login are valid"""
         try:
-            self._db.find_user_by(email=email)
-        except NoResultFound:
-            hashed_password = _hash_password(password)
-            new_user = self._db.add_user(email=email,
-                                         hashed_password=hashed_password)
-            return new_user
-        else:
-            raise ValueError(f"User {email} already exists")
+            user = self._db.find_user_by(email=email)
+        except Exception as e:
+            return False
+
+        if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+            return True
+        return False
