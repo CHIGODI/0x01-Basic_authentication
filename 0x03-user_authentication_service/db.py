e6dd4179d2ec5b736e97cd63c681ac8d804bd112
CHIGODI <chiegody254@gmail.com>
2024-08-14 08:55:39 +0300

    Remove input validation in add_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,11 +36,10 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add user to db"""
-        if isinstance(str, email) and isinstance(str, hashed_password):
-            user: User = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
-            return user
+        user: User = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
 
     def find_user_by(self, **kwargs) -> User:
         """Find user in db"""
