223671216f841eef2f3c86ddd66e3d1f8cb64b3c
CHIGODI <chiegody254@gmail.com>
2024-08-15 11:39:23 +0300

    Add type annotation to init

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,7 +29,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields: List[str]):
         """Init"""
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
