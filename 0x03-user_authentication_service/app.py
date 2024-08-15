da33303dac6f016b83c7e6daaaceed437c98ee10
CHIGODI <chiegody254@gmail.com>
2024-08-15 13:41:58 +0300

    Add update_password PUT

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,7 +84,7 @@ def profile():
         abort(403)
 
 
-@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+@app.route('/reset_password', methods=['POST', 'PUT'], strict_slashes=False)
 def reset_password():
     """reset password"""
     email = request.form.get('email')
@@ -94,6 +94,23 @@ def reset_password():
     try:
         reset_token = AUTH.get_reset_password_token(email=email)
         return jsonify({"email": email, "reset_token": reset_token})
+    except Exception:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """update password"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if not email or not reset_token or not new_password:
+        abort(403)
+
+    try:
+        reset_token = AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
         abort(403)
 
